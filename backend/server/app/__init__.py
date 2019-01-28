9d39ac056d5e852a9afd7f03cf819459fad85586
Joseph Keatinge <joseph260198@gmail.com>
2019-01-28 21:40:16 +0000

    Cleaned up git tracking and added basic api code

--- backend/server/app/__init__.py
+++ backend/server/app/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask
-#from spark-commands import *
+from flask import Flask, g
+from flask_restful import Resource, Api, reqparse
+import os
 
 app = Flask(__name__)
 
-from app import routes
+api = Api(app)
+
+class DFHead(Resource):
+    def get(self, identifier):
+        return {'data': identifier}, 200
+
+api.add_resource(DFHead, '/dataset/<string:identifier>')
