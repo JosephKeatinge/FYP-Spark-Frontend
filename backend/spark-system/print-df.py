d0b7d90422609b769b9abf2c4db3a893f1fae803
Joseph Keatinge <joseph260198@gmail.com>
2019-03-15 20:50:59 +0000

    Cleaned up script to start HDFS and python script.

--- backend/spark-system/print-df.py
+++ backend/spark-system/print-df.py
@@ -2,7 +2,6 @@ from pyspark.sql import SparkSession
 import sys
 
 file_uri = "hdfs://localhost:9000/datasets/%s" % (sys.argv[1])
-
 spark = SparkSession.builder.appName("BasicApp").getOrCreate()
 
 df = spark.read.option("header", "true").option("inferSchema", "true").csv(file_uri)
@@ -10,9 +9,11 @@ df = spark.read.option("header", "true").option("inferSchema", "true").csv(file_
 file_title = sys.argv[1].split(".")[0]
 sql_query = sys.argv[2]
 
-spark.createOrReplaceTempView(file_title)
+print("About to execute query %s on table %s" % (sql_query, file_title))
+
+df.createOrReplaceTempView(file_title)
 df2 = spark.sql(sql_query)
 
-df2.write.mode("overwrite").json("script-output/print-df/%s" % file_title)
+df2.write.mode("overwrite").json("hdfs://localhost:9000/output/%s" % file_title)
 
 spark.stop()
